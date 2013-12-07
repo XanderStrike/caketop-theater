893bd0b71919da04148a9e6d32759782619551dd
Alexander Standke <xanderstrike@gmail.com>
2013-12-07 14:59:11 -0800

    changes

--- create_db.rb
+++ create_db.rb
@@ -4,7 +4,6 @@ require 'themoviedb'
 
 # config
 Tmdb::Api.key("a230f1c8a13699563ac819f74fb16230")
-db = SQLite3::Database.new("data.db")
 
 # methods
 def clean_title(title)
@@ -21,39 +20,54 @@ def get_info(id)
   return Tmdb::Movie.detail(id)
 end
 
-# get list of files, run queries
-files = `find /media/nasdrive/movies -maxdepth 1 -type f -printf '%f\n'`.split("\n")
-files.each do |file|
-  puts
-  puts "Searching for " + file
-  if db.get_first_value("select count(1) from movies where filename = ?", file) > 0
-    puts "Already exists in DB, skipping..."
-  else
-    movie = clean_title(file)
-    if movie == false
-      puts "Title not found '#{file}', skipping..."
-      next
+def populate_db(files)
+  db = SQLite3::Database.new("data.db")
+
+  files.each do |file|
+    puts
+    puts "Searching for " + file
+    if db.get_first_value("select count(1) from movies where filename = ?", file) > 0
+      puts "Already exists in DB, skipping..."
+    else
+      movie = clean_title(file)
+      if movie == false
+        puts "Title not found '#{file}', skipping..."
+        next
+      end
+      puts movie.title
+      info = get_info(movie.id)
+      puts info.overview
+      db.execute("insert into movies values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
+                    info.backdrop_path,
+                    info.budget,
+                    info.id,
+                    info.imdb_id,
+                    info.original_title,
+                    info.overview,
+                    info.popularity,
+                    info.poster_path,
+                    info.release_date,
+                    info.revenue,
+                    info.runtime,
+                    info.status,
+                    info.tagline,
+                    info.title,
+                    info.vote_average,
+                    info.vote_count,
+                    file)
     end
-    puts movie.title
-    info = get_info(movie.id)
-    puts info.overview
-    db.execute("insert into movies values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
-                  info.backdrop_path,
-                  info.budget,
-                  info.id,
-                  info.imdb_id,
-                  info.original_title,
-                  info.overview,
-                  info.popularity,
-                  info.poster_path,
-                  info.release_date,
-                  info.revenue,
-                  info.runtime,
-                  info.status,
-                  info.tagline,
-                  info.title,
-                  info.vote_average,
-                  info.vote_count,
-                  file)
   end
 end
+
+# get list of files, run queries
+puts "Scanning library for new files..."
+files = `find /media/nasdrive/movies -maxdepth 1 -type f -printf '%f\n'`.split("\n")
+populate_db(files)
+
+puts "\nRemoving missing files..."
+db = SQLite3::Database.new("data.db")
+dbfiles = db.execute("select filename from movies")
+puts "Missing files:"
+missing = dbfiles.map(&:first) - files
+puts missing
+missing.each {|f| db.execute("delete from movies where filename = ?", f)}
