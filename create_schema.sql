dfb4ffa4fa2b60ee827892d195b13e57c0612559
Alexander Standke <xanderstrike@gmail.com>
2014-01-15 15:57:05 -0800

    cleaning

--- create_schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE movies(backdrop_path text, budget integer, id integer, imdb_id text, original_title text, overview text, popularity text, poster_path text, release_date text, revenue integer, runtime integer, status text, tagline text, title text, vote_average text, vote_count text, filename text);
-
-CREATE TABLE recent(filename text, watched_id integer, time integer, ip text);
-
-CREATE TABLE requests(name text, request text, status text);
