81badd830290eaa9a34f5cd984fb20d1a8e2aa7d
Alexander Standke <xanderstrike@gmail.com>
2014-03-05 13:24:51 -0800

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaketopTheater::Application.config.secret_token = 'bbf018e5fef80229007e600c3684a8a2012e886f4cdf3506dcf7369fe28dd8b71c9448404e4d24ec0b4fe4263dbd012d562ba000f82f8242403ff251c515d9e5'
