522bb79c0d36f48190bb25d321ab77d0ac96e3ec
Alexander Standke <xanderstrike@gmail.com>
2014-03-05 20:06:27 -0800

    add movie show page

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,8 @@
 class Movie < ActiveRecord::Base
   attr_accessible :added, :backdrop_path, :budget, :filename, :id, :imdb_id, :original_title, :overview, :popularity, :poster_path, :release_date, :revenue, :runtime, :status, :tagline, :title, :vote_average, :vote_count
 
+  has_many :genres
+
   def poster
     "posters/#{self.id}.jpg"
   end
