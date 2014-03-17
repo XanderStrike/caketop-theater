3840c98f9af7b3968f39444f6da6766880faade5
Alexander Standke <xanderstrike@gmail.com>
2014-03-17 16:27:14 -0700

    add shows index

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
   attr_accessible :backdrop_path, :first_air_date, :folder, :id, :name, :original_name, :overview, :popularity, :poster_path, :vote_average, :vote_count
+
+  def poster
+    "posters/tv_#{self.id}.jpg"
+  end
+
+  def backdrop
+    "backdrops/tv_#{self.id}.jpg"
+  end
 end
