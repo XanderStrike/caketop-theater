d01cead027d12567f4cd77fe2ae1ea95fe89cf59
Alexander Standke <xanderstrike@gmail.com>
2014-03-19 14:36:28 -0700

    add tv show search

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -27,63 +27,16 @@ class ShowsController < ApplicationController
     end
   end
 
-  # GET /shows/new
-  # GET /shows/new.json
-  def new
-    @show = Show.new
+  def search
+    @results = Show.where("name like ?", "%#{params[:q]}%")
+    @results += Show.where("original_name like ?", "%#{params[:q]}%")
+    @results += Show.where("folder like ?", "%#{params[:q]}%")
+    @results += Show.where("overview like ?", "%#{params[:q]}%")
+    @results = @results.uniq
 
     respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @show }
-    end
-  end
-
-  # GET /shows/1/edit
-  def edit
-    @show = Show.find(params[:id])
-  end
-
-  # POST /shows
-  # POST /shows.json
-  def create
-    @show = Show.new(params[:show])
-
-    respond_to do |format|
-      if @show.save
-        format.html { redirect_to @show, notice: 'Show was successfully created.' }
-        format.json { render json: @show, status: :created, location: @show }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @show.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /shows/1
-  # PUT /shows/1.json
-  def update
-    @show = Show.find(params[:id])
-
-    respond_to do |format|
-      if @show.update_attributes(params[:show])
-        format.html { redirect_to @show, notice: 'Show was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @show.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /shows/1
-  # DELETE /shows/1.json
-  def destroy
-    @show = Show.find(params[:id])
-    @show.destroy
-
-    respond_to do |format|
-      format.html { redirect_to shows_url }
-      format.json { head :no_content }
+      format.html # show.html.erb
+      format.json { render json: @movie }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 CaketopTheater::Application.routes.draw do
-
-  resources :shows
-
-
   get '/', to: 'home#index'
   get '/theater', to: 'home#index'
 
   resources :genres
-  
+
   post 'movies/search'
   get 'movies/shuffle'
   resources :movies
 
   post 'encodes/find_movie'
   get 'encodes/retag'
+
+  post 'shows/search'
+  resources :shows
 end
