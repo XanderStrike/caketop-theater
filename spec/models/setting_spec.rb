f3b9b76679d2aa26be218f6091876a7642b80805
Alexander Standke <xanderstrike@gmail.com>
2014-11-19 09:47:31 -0800

    add spec for settings modelg

---
+++ spec/models/setting_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Setting, :type => :model do
+  describe '.get' do
+    it 'should get the setting by its name' do
+      setting = Setting.new(name: 'testsetting')
+      setting.save
+      expect(Setting.get('testsetting')).to eq(setting)
+    end
+  end
+
+  describe '.render' do
+    context 'with boolean set to true' do
+      it 'should render the contents' do
+        setting = Setting.new(name: 'testsetting', boolean: true, content: 'content')
+        setting.save
+        expect(Setting.render('testsetting')).to eq('content')
+      end
+    end
+
+    context 'with boolean set to false' do
+      it 'should render the contents' do
+        setting = Setting.new(name: 'testsetting', boolean: false, content: 'content')
+        setting.save
+        expect(Setting.render('testsetting')).to eq('')
+      end
+    end
+  end
+end
