20cd283a90bcfa7b5048758f3ebf0f6cfc805b31
Alexander Standke <xanderstrike@gmail.com>
2014-11-19 21:03:29 -0800

    add encode spec

---
+++ spec/factories/encode.rb
@@ -0,0 +1,22 @@
+FactoryGirl.define do
+  factory :encode do
+    a_bitrate "192 kbps"
+    a_format "Advanced Audio Codec"
+    a_stream_size "189 MiB (9%)"
+    aspect_ratio "2.40:1"
+    container "MPEG-4"
+    duration "2h 14mn"
+    framerate "23.976 fps"
+    resolution "1280"
+    rip_date "#{Time.now}"
+    size "2210466750"
+    v_bitrate "2,000 kbps"
+    v_codec "avc1"
+    v_format "AVC"
+    v_profile "High@L3.1"
+    v_stream_size "1.87 GiB (91%)"
+    sequence(:filename) {|n| "Movie #{n}.2013.720p.BR-Rip.mp4"}
+
+    movie
+  end
+end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe Movie, :type => :model do
   describe '#poster' do
     it 'should return the path to the poster' do
-      movie = Movie.new(id: 1234)
-      expect(movie.poster).to eq('/posters/1234.jpg')
+      movie = build(:movie)
+      expect(movie.poster).to eq("/posters/#{movie.id}.jpg")
     end
   end
 
   describe '#backdrop' do
     it 'should return the path to the packdrop' do
-      movie = Movie.new(id: 1234)
-      expect(movie.backdrop).to eq('/backdrops/1234.jpg')
+      movie = build(:movie)
+      expect(movie.backdrop).to eq("/backdrops/#{movie.id}.jpg")
     end
   end
 end
