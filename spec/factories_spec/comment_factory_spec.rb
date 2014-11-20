494946cb052dee99bff708f922c283ec2761669e
Alexander Standke <xanderstrike@gmail.com>
2014-11-20 09:17:39 -0800

    make tests pass

--- spec/factories_spec/comment_factory_spec.rb
+++ spec/factories_spec/comment_factory_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Comment Factory', type: :factory do
 
   describe "comment" do
     it 'has all the attributes from the model' do
-      expect(attributes_for(:comment)).to include(*(factory_attributes(Comment) - [:movie_id]))
+      expect(attributes_for(:comment)).to include(*(factory_attributes(Comment) - [:movie_id, :content]))
     end
 
     it 'produces a valid model' do
