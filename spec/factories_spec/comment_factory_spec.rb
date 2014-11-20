506832b7b40c35f18b967171401736bbbb5f8797
Alexander Standke <xanderstrike@gmail.com>
2014-11-19 21:13:02 -0800

    add comment factory

---
+++ spec/factories_spec/comment_factory_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Comment Factory', type: :factory do
+  include FactoryGirl::Syntax::Methods
+
+  describe "comment" do
+    it 'has all the attributes from the model' do
+      expect(attributes_for(:comment)).to include(*(factory_attributes(Comment) - [:movie_id]))
+    end
+
+    it 'produces a valid model' do
+      expect(build(:comment)).to be_valid
+    end
+  end
+end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe Comment, :type => :model do
   describe '#body_html' do
     it 'should return the body of the comment in markdown-parsed html' do
-      comment = Comment.new(body: "#a \n\n b")
-      expect(comment.body_html).to eq("<h1>a</h1>\n\n<p>b</p>\n")
+      comment = build(:comment)
+      expect(comment.body_html).to eq("<p>I love this movie!</p>\n")
     end
   end
 end
