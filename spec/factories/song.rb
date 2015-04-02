a6a64758b852430aa3e78aed34d997ca27f9dfd6
Adam Hess <adamhess1991@gmail.com>
2015-04-02 09:54:40 -0700

    update factory and put js back

--- spec/factories/song.rb
+++ spec/factories/song.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :song do
-    sequence(:filename) {|n| "music/Artist/Album/Track #{n}.mp3"}
+    sequence(:filename) {|n| "Track #{n}.mp3"}
     sequence(:title) {|n| "Track #{n}"}
     sequence(:track) {|n| n}
-    
+
     album
   end
 end
