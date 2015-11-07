e5f98b53288aa2d3f0048c7e4a37adc50c714401
Alexander Standke <xanderstrike@gmail.com>
2015-11-06 20:09:54 -0800

    update movie scan task for new tmdb gem

--- app/services/tmdb_service.rb
+++ app/services/tmdb_service.rb
@@ -1,19 +1,51 @@
 class TmdbService
+  class TmdbError < StandardError
+  end
+
   class << self
+    def find_title_by_filename(filename)
+      filename = filename.split('/').last.split(/[\s\.]/)
+      filename.size.times do |x|
+        next if x == 0
+        results = search_movie_title(filename[0...(filename.length - x)].join(' '))
+        return results[0] if results.size > 0
+      end
+      false
+    end
+
     def search_movie_title(title)
-      Tmdb::Movie.find(title)
+      handle_status_codes do
+        Tmdb::Movie.find(title)
+      end
     end
 
     def get_movie_info(id)
-      Tmdb::Movie.detail(id)
+      handle_status_codes do
+        Tmdb::Movie.detail(id)
+      end
     end
 
     def search_tv_title(title)
-      Tmdb::TV.find(title)
+      handle_status_codes do
+        Tmdb::TV.find(title)
+      end
     end
 
     def get_tv_info(id)
-      Tmdb::TV.detail(id)
+      handle_status_codes do
+        Tmdb::TV.detail(id)
+      end
+    end
+
+    private
+
+    def handle_status_codes(&block)
+      response = yield(block)
+      if response.is_a?(Hash) && response['status_code'].present?
+        raise TmdbError, response['message']
+      else
+        response
+      end
     end
   end
 end
